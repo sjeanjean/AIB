781770fd9b1e0906a9d5086ccd7a6de9aafcf78b
stephjj <sjeanjean@yahoo.fr>
2021-07-19 10:12:03 +0200

    Add CustomizeImage and InstallaApplications

---
+++ Artifacts/CustomizeImage.ps1
@@ -0,0 +1,26 @@
+#region Set logging 
+$logFile = "c:\temp\" + (get-date -format 'yyyyMMdd') + '_softwareinstall.log'
+function Write-Log {
+    Param($message)
+    Write-Output "$(get-date -format 'yyyyMMdd HH:mm:ss') $message" | Out-File -Encoding utf8 $logFile -Append
+}
+#endregion
+
+#region Time Zone Redirection
+$Name = "fEnableTimeZoneRedirection"
+$value = "1"
+# Add Registry value
+try {
+    New-ItemProperty -ErrorAction Stop -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Name $name -Value $value -PropertyType DWORD -Force
+    if ((Get-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services").PSObject.Properties.Name -contains $name) {
+        Write-log "Added time zone redirection registry key"
+    }
+    else {
+        write-log "Error locating the Teams registry key"
+    }
+}
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-log "Error adding teams registry KEY: $ErrorMessage"
+}
+#endregion
---
+++ Artifacts/InstallApplications.ps1
@@ -0,0 +1,23 @@
+#region Set logging 
+$logFile = "c:\temp\" + (get-date -format 'yyyyMMdd') + '_softwareinstall.log'
+function Write-Log {
+    Param($message)
+    Write-Output "$(get-date -format 'yyyyMMdd HH:mm:ss') $message" | Out-File -Encoding utf8 $logFile -Append
+}
+#endregion
+
+#region Install OneDrive
+try {
+    Start-Process -FilePath 'c:\temp\OneDriveSetup.exe' -Wait -ErrorAction Stop -ArgumentList '/silent', '/allusers'
+        if (Test-Path "C:\Program Files (x86)\Microsoft OneDrive\OneDrive.exe") {
+        Write-Log "OneDrive has been installed"
+    }
+    else {
+        write-log "Error locating the OneDrive executable"
+    }
+}
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-log "Error installing OneDrive: $ErrorMessage"
+}
+#endregion
