d65136fdddb8b1c44e64c931824e747bd3cfdc01
stephjj <sjeanjean@yahoo.fr>
2021-07-21 14:33:46 +0200

    Add ActiveSetup for UserLanguageList

--- Artifacts/CustomizeImage.ps1
+++ Artifacts/CustomizeImage.ps1
@@ -7,20 +7,21 @@ function Write-Log {
 #endregion
 
 #region Time Zone Redirection
+$RPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
 $Name = "fEnableTimeZoneRedirection"
 $value = "1"
 # Add Registry value
 try {
-    New-ItemProperty -ErrorAction Stop -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Name $name -Value $value -PropertyType DWORD -Force
-    if ((Get-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services").PSObject.Properties.Name -contains $name) {
-        Write-log "Added time zone redirection registry key"
+    New-ItemProperty -ErrorAction Stop -Path $RPath -Name $name -Value $value -PropertyType DWORD -Force
+    if ((Get-ItemProperty $RPath).PSObject.Properties.Name -contains $name) {
+        Write-log "Added $Name registry key"
     }
     else {
-        write-log "Error locating the Teams registry key"
+        write-log "Error locating the $Name registry key"
     }
 }
 catch {
     $ErrorMessage = $_.Exception.message
-    write-log "Error adding teams registry KEY: $ErrorMessage"
+    write-log "Error adding $Name registry KEY: $ErrorMessage"
 }
 #endregion
--- Artifacts/Install-LanguagePacks.ps1
+++ Artifacts/Install-LanguagePacks.ps1
@@ -2,15 +2,25 @@
 ## Add Languages to running Windows Image for Capture##
 ########################################################
 
+#region Set logging 
+$logFile = $env:SystemRoot + "\Temp\" + (get-date -format 'yyyyMMdd') + 'Install-LanguagesPacks.log'
+function Write-Log {
+    Param($message)
+    Write-Output "$(get-date -format 'yyyyMMdd HH:mm:ss') $message" | Out-File -Encoding utf8 $logFile -Append
+}
+#endregion
+
 ##Disable Language Pack Cleanup##
 Disable-ScheduledTask -TaskPath "\Microsoft\Windows\AppxDeploymentClient\" -TaskName "Pre-staged app cleanup"
 
-##Copy language Repository
-azcopy copy https://bycnitaibsources.blob.core.windows.net/languagepacks/21h1/LanguagesPacks.zip c:\temp
-Expand-Archive C:\temp\LanguagesPacks.zip -DestinationPath c:\temp\LanguagesPacks
-
 ##Set Language Pack Content Stores##
 [string]$LIPContent = "C:\temp\LanguagesPacks"
+
+##Copy language Repository
+azcopy copy https://bycnitaibsources.blob.core.windows.net/languagepacks/21h1/LanguagesPacks.zip c:\temp
+Expand-Archive C:\temp\LanguagesPacks.zip -DestinationPath $LIPContent
+
+
 
 ##French##
 Add-AppProvisionedPackage -Online -PackagePath $LIPContent\fr-fr\LanguageExperiencePack.fr-fr.Neutral.appx -LicensePath $LIPContent\fr-fr\License.xml
@@ -29,3 +39,46 @@ Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-LanguageFe
 Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-LanguageFeatures-OCR-de-de-Package~31bf3856ad364e35~amd64~~.cab
 Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-LanguageFeatures-Speech-de-de-Package~31bf3856ad364e35~amd64~~.cab
 Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-LanguageFeatures-TextToSpeech-de-de-Package~31bf3856ad364e35~amd64~~.cab
+
+#region Block clean-up of unused language packs
+$RPath = "HKLM:\SOFTWARE\Policies\Microsoft\Control Panel\International"
+$Name = "BlockCleanupOfUnusedPreinstalledLangPacks"
+$value = "1"
+# Add Registry value
+try {
+    New-ItemProperty -ErrorAction Stop -Path $RPath -Name $name -Value $value -PropertyType DWORD -Force
+    if ((Get-ItemProperty $RPath).PSObject.Properties.Name -contains $name) {
+        Write-log "Added $Name registry key"
+    }
+    else {
+        write-log "Error locating the $Name registry key"
+    }
+}
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-log "Error adding $Name registry KEY: $ErrorMessage"
+}
+#endregion
+
+##region Add ActiveSetup for configure UserLanguageList
+try {
+    New-Item -Path Registry::'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components' -Name 'OSD-ConfigurePreferredUserLangages'
+    New-ItemProperty -Path Registry::'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\OSD-ConfigurePreferredUserLangages' `
+                     -Name 'Version' `
+                     -Value '1,0,0,0' `
+                     -PropertyType 'String'
+    New-ItemProperty -Path Registry::'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\OSD-ConfigurePreferredUserLangages' `
+                     -Name 'StubPath' `
+                     -Value 'Powershell.exe -ExecutionPolicy bypass -WindowStyle Hidden -NonInteractive -command "$list=Get-WinUserLanguageList; $list.Add(''de-ch''); $list.Add(''de-de''); $list.Add(''fr-ch''); $list.Add(''it-ch''); $list.Add(''it-it''); $list.Add(''fr-fr''); Set-WinUserLanguageList -LanguageList $list -Force"' `
+                     -PropertyType 'String'
+    if ((Get-ItemProperty $Registry::'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\OSD-ConfigurePreferredUserLangages').PSObject.Properties.Name -contains 'Version') {
+        Write-log "Added Active Setup registry key"
+    }
+    else {
+        write-log "Error locating the Active Setup registry key"
+    }
+}
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-log "Error adding Active Setup registry KEY: $ErrorMessage"
+}#endregion
