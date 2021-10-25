3c35e83a844ffbbf98e84409070760591b61e3df
stephjj <sjeanjean@yahoo.fr>
2021-10-25 11:58:07 +0200

    Disable Windows Store

--- Artifacts/CustomizeImage.ps1
+++ Artifacts/CustomizeImage.ps1
@@ -29,3 +29,23 @@ catch {
 #region Disable Windows Search
 Set-Service -Name WSearch -StartupType Disabled
 #endregion
+
+#region Disable Windows Store
+$RPath = "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore"
+$Name = "RemoveWindowsStore"
+$value = "1"
+# Add Registry value
+try {
+    New-ItemProperty -ErrorAction Stop -Path $RPath -Name $name -Value $value -PropertyType DWORD -Force
+    if ((Get-ItemProperty $RPath).PSObject.Properties.Name -contains $name) {
+        Write-log "Added $Name registry key"
+    }
+    else {
+        write-log "Error locating the $Name registry key"
+    }
+}
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-log "Error adding $Name registry KEY: $ErrorMessage"
+}
+#endregion
